ab0015d011088c382ca6f0e95f49d8fcb8e00f35
Joel Abrahams <abrahamsjo@gmail.com>
2020-03-11 20:49:32 +0100

    Ad traceroute
    I still can't display these routes. An issue
    is that I can't add traces to plotly after
    a call show() on the figure.
    
    Another issue is that even if I create an
    entirely new figure with the new traces, I see
    them in the legend but not on the map itself.
    I should investigate why this is.
    
    Another thing of not is that traceroute does
    not return a list of ip addresses.

--- src/main.py
+++ src/main.py
@@ -7,11 +7,10 @@ from threading import Thread
 from typing import Optional, Tuple
 
 from scapy.all import sniff
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, traceroute
 from scapy.packet import Packet
 
 import plotly.graph_objects as go
-import plotly.express as px
 
 seen_global_ips = set()
 recent_ips = set()
@@ -29,9 +28,8 @@ def get_lat_lon(ip: str) -> Optional[Tuple[float, float]]:
     elif ip in ip_locations:
         return ip_locations[ip]
 
-    with urllib.request.urlopen(f"https://geolocation-db.com/json/{ip}") as url:
+    with urllib.request.urlopen(f'https://geolocation-db.com/json/{ip}') as url:
         json_data = json.loads(url.read().decode())
-
         if 'latitude' not in json_data or 'longitude' not in json_data:
             blacklisted_ips.add(ip)
             return None
@@ -55,17 +53,20 @@ class AddFig(Thread):
 
         lats, lons = [], []
         for ip in self.ips:
-            res = get_lat_lon(ip)
-            if res is not None:
-                lat, lon = res[0], res[1]
-                logging.info(msg=f"{ip} at {lat},{lon}")
+            route, unans = traceroute(ip, maxttl=32)
+            msg = f'Route to {ip}: '
 
-                lats += [lat]
-                lons += [lon]
+            for traced_ip in route.get_trace():
+                res = get_lat_lon(traced_ip)
+                if res is not None:
+                    lat, lon = res[0], res[1]
+                    lats += [lat]
+                    lons += [lon]
+                    msg += f'{traced_ip} [{lat}, {lon}], '
 
-        # print(ip_set_data)
-        fig.add_trace(go.Scattermapbox(mode="markers", lon=lons, lat=lats, marker={'size': 10}))
-        fig.update_layout()
+            if len(lats) > 0:
+                fig.add_trace(go.Scattermapbox(mode='markers+lines', lon=lons, lat=lats, marker={'size': 10}))
+                logging.info(msg)
 
 
 def dns_display(pkt: Packet):
@@ -73,9 +74,7 @@ def dns_display(pkt: Packet):
         return
 
     dst = pkt[IP].dst
-    if not ipaddress.ip_address(dst).is_global:
-        return
-    elif dst in seen_global_ips:
+    if not ipaddress.ip_address(dst).is_global or dst in seen_global_ips:
         return
 
     seen_global_ips.add(dst)
@@ -83,16 +82,23 @@ def dns_display(pkt: Packet):
 
     if len(seen_global_ips) % update_interval == 0:
         add_fig = AddFig(recent_ips)
-        add_fig.run()
-
         recent_ips.clear()
+        add_fig.run()
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename=f'{time.strftime("%Y-%m-%d-%H%M%S")}.log', level=logging.INFO)
+    fig = go.Figure(go.Scattergeo())
+    fig.update_geos(
+        visible=True,
+        resolution=50,
+        showcountries=True,
+        countrycolor="Black"
+    )
 
-    fig = px.scatter_geo()
-    fig.update_layout(margin={'l': 1, 't': 1, 'b': 1, 'r': 1}, mapbox={'style': "open-street-map", })
+    fig.update_layout(
+        margin={'l': 0, 't': 30, 'b': 0, 'r': 0},
+    )
+
     fig.show()
-
+    logging.basicConfig(filename=f'{time.strftime("%Y-%m-%d-%H%M%S")}.log', level=logging.INFO)
     sniff(prn=dns_display)
